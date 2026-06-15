e591e0ca2e8b912b7d9254d35eb3532825105fa1
kaarthikeyan <kaarthikeyanm120105@gmail.com>
2026-06-15 09:32:52 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ connectDB();
 const app = express();
 
 app.use(cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:5173',
+    origin: "https://ev-station-slot-booking-frontend.onrender.com",
     credentials: true,
 }));
 app.use(express.json({ limit: '50mb' }));
